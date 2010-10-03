afee203ffcf8337f2587ed4baf271ef9e993b78b
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2010-10-03 16:53:10 +0530

    Adding Buildout build system

---
+++ setup.py
@@ -0,0 +1,29 @@
+import sys
+from setuptools import setup, find_packages
+
+name='pypdflib'
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description=(
+        read('README')
+        + '\n' 
+        )
+
+setup(
+    name=name,
+    version='1.0.0',
+    url='http://github.com/santhoshtr/pypdflib',
+    license='LGPL 3.0',
+    description='PangoCairo based Python PDF Library',
+    author='Santhosh Thottingal',
+    author_email='santhosh.thottingal@gmail.com',
+    long_description=long_description,
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages=['pypdflib'],
+    include_package_data = True,
+    install_requires=['setuptools'],
+    zip_safe = False,
+    )
---
+++ src/pypdflib/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+__import__('pkg_resources').declare_namespace(__name__)
