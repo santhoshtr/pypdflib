e4294e72098b0a8391bec9a3c807391054e8afba
Vasudev Kamath <kamathvasudev@gmail.com>
2010-10-05 21:26:41 +0530

    README file name changed to README.md

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 long_description=(
-        read('README')
+        read('README.md')
         + '\n' 
         )
 
