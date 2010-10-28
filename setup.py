354d168921c7094afe8561d47b18b1d31e338e6a
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2010-10-28 21:57:14 +0530

    Adding License Headers

--- setup.py
+++ setup.py
@@ -1,3 +1,24 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+# pypdflib is a pango/cairo framework for generating reports.
+# Copyright © 2010  Santhosh Thottingal <santhosh.thottingal@gmail.com>
+
+# This file is part of pypdflib.
+#
+# pypdflib is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.  
+#
+# pypdflib is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with pypdflib.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 import os
 from setuptools import setup, find_packages
@@ -14,7 +35,7 @@ long_description=(
 
 setup(
     name=name,
-    version='1.0.0',
+    version='0.1.a1',
     url='http://github.com/santhoshtr/pypdflib',
     license='LGPL 3.0',
     description='PangoCairo based Python PDF Library',
